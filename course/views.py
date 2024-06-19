83fcafbf2870389a4211fc7729e1e8d51dda69d2
Majumder99 <souravmajumderk99@gmail.com>
2024-06-19 14:08:07 +0600

    learned views

--- course/views.py
+++ course/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def learn_django(request):
+    return HttpResponse("This is the learn_django view")
+
+def index(request):
+    return HttpResponse("This is the index view")
--- introtodjango/urls.py
+++ introtodjango/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from course import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("django/", views.learn_django),
+    path("", views.index)
 ]
