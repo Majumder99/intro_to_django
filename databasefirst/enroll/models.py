c9ac6c76f630e75891955b53fc3d3160fae02067
Majumder99 <souravmajumderk99@gmail.com>
2024-06-24 18:16:47 +0600

    learned basic of models

---
+++ databasefirst/enroll/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    studid=models.IntegerField()
+    stuname=models.CharField(max_length=30)
+    stuemail=models.EmailField()
+    stupass=models.CharField(max_length=30)
+    common=models.CharField(max_length=30, default='common')
