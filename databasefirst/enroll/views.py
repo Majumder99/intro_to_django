c7026dcdc746769e15a2c6764dc091a5837c970a
Majumder99 <souravmajumderk99@gmail.com>
2024-06-25 16:54:50 +0600

    fetch database and admin page

--- databasefirst/enroll/views.py
+++ databasefirst/enroll/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from enroll.models import Student
 
 # Create your views here.
+def show_data(request):
+    students = Student.objects.all()
+    print('data from database', students)
+    return render(request, 'showdata.html', context={'stus': students})
