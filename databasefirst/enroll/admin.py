840145761537be05bb200d1974028a5478ebf148
Majumder99 <souravmajumderk99@gmail.com>
2024-06-25 17:10:46 +0600

    model class

--- databasefirst/enroll/admin.py
+++ databasefirst/enroll/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-
 from enroll.models import Student
 
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'stuname', 'stuemail')
+
 # Register your models here.
-admin.site.register(Student)
+admin.site.register(Student, StudentAdmin)
+
